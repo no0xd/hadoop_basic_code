7d71ca731e3d5ed1fa47bdcb6448ced849a55350
贺靖轩 <hejingxuan@netposa.com>
2019-08-05 16:01:40 +0800

    add counter

--- mr/src/main/java/com/netposa/mr2/SortMapper.java
+++ mr/src/main/java/com/netposa/mr2/SortMapper.java
@@ -3,6 +3,7 @@ package com.netposa.mr2;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.util.StringUtils;
 
@@ -12,6 +13,12 @@ public class SortMapper extends Mapper<LongWritable,Text,SoftPairWritable,NullWr
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        //自定义计数器
+        Counter imyMrCount = context.getCounter("MR_COUNT", "ImyMRCount");
+        imyMrCount.increment(1L);
+
+
         String[] split = StringUtils.split(value.toString(), '\t');
         System.out.println("split[0]=>"+split[0]);
         System.out.println("split[1]=>"+split[1]);
--- mr/src/main/java/com/netposa/mr2/SortReducer.java
+++ mr/src/main/java/com/netposa/mr2/SortReducer.java
@@ -6,8 +6,22 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 public class SortReducer extends Reducer<SoftPairWritable,NullWritable,SoftPairWritable,NullWritable>{
+
+    //自定义计数器:使用枚举
+
+    public static enum MyCounter{
+        REDUCER_INPUT_KEY_RECOREDS,     //输入key统计
+        REDUCER_INPUT_VALUE_RECOREDS    //输入value统计
+    }
+
     @Override
     protected void reduce(SoftPairWritable key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
-        super.reduce(key, values, context);
+
+        context.getCounter(MyCounter.REDUCER_INPUT_KEY_RECOREDS).increment(1L);
+
+        for (NullWritable val:values){
+            context.getCounter(MyCounter.REDUCER_INPUT_VALUE_RECOREDS).increment(1L);
+            context.write(key,NullWritable.get());
+        }
     }
 }
